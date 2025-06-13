58983741e8043595b9d0723983432f19f2b115ff
VarsityPlug0 <VarsityPlug0@gmail.com>
2025-06-13 15:01:14 +0200

    Add company banking details to deposit and withdrawal instructions

---
+++ Cryptovault3.0/core/banking_config.py
@@ -0,0 +1,29 @@
+"""
+Banking configuration for SafeChain Vault
+"""
+
+# Company Banking Details
+COMPANY_BANK_DETAILS = {
+    'account_number': '63160510955',
+    'bank_name': 'FNB',
+    'account_name': 'SafeChain Vault',
+    'branch_code': '250655',  # FNB branch code
+    'account_type': 'Business Account'
+}
+
+# Bank Branch Codes
+BANK_BRANCH_CODES = {
+    'ABSA': '632005',
+    'CAPITEC': '470010',
+    'FNB': '250655',
+    'INVESTEC': '580105',
+    'NEDBANK': '198765',
+    'STANDARD': '051001',
+    'AFRICAN': '430000',
+    'BIDVEST': '462005',
+    'DISCOVERY': '679000',
+    'GRINDROD': '584000',
+    'HSBC': '587000',
+    'MERCANTILE': '450905',
+    'TYME': '678910'
+} 
